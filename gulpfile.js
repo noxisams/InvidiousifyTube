feb0623308e824742cbc7491269442f48d3f6fe9
Noxisams <73168161+noxisams@users.noreply.github.com>
2023-11-04 12:02:13 +0100

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const htmlMin = require('gulp-htmlmin');
+const cssMin = require('gulp-clean-css');
+const jsMin = require('gulp-terser');
+const zip = require('gulp-zip');
+
+const folderApp = 'app/';
+const folderBuild = 'dist/';
+
+// Tâche pour copier tous les fichiers
+gulp.task('copy', function () {
+    return gulp.src(folderApp + '**/*')
+        .pipe(gulp.dest(folderBuild + folderApp));
+});
+
+// Tâche pour minifier les fichiers HTML
+gulp.task('minify-html', () => {
+    return gulp.src(folderApp + '*.html')
+        .pipe(htmlMin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(folderBuild + folderApp));
+});
+
+// Tâche pour minifier les fichiers CSS
+gulp.task('minify-css', () => {
+    return gulp.src(folderApp + '*.css')
+        .pipe(cssMin())
+        .pipe(gulp.dest(folderBuild + folderApp));
+});
+
+// Tâche pour minifier les fichiers JavaScript
+gulp.task('minify-js', () => {
+    return gulp.src(folderApp + '*.js')
+        .pipe(jsMin())
+        .pipe(gulp.dest(folderBuild + folderApp));
+});
+
+// Tâche pour compresser tous les fichiers minifiés dans un fichier ZIP
+gulp.task('zip', () => {
+    const packageVersion = require('./package.json').version;
+    return gulp.src(folderBuild + folderApp + '**/*')
+        .pipe(zip('app_v' + packageVersion +'.zip'))
+        .pipe(gulp.dest(folderBuild));
+});
+
+// Tâche par défaut pour exécuter toutes les tâches
+gulp.task('default', gulp.series('copy', 'minify-html', 'minify-css', 'minify-js', 'zip'));
